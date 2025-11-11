acc45489f4e6cfb9a80a3cf9e3f68b6214c74445
sakib2077 <54476615+sakib2077@users.noreply.github.com>
2025-11-11 15:16:03 +0530

    added odia video

--- src/App.tsx
+++ src/App.tsx
@@ -412,16 +412,7 @@ function App() {
             
             {/* YouTube Video */}
             <div className="mt-6">
-              <iframe 
-                width="560" 
-                height="315" 
-                src="https://www.youtube.com/embed/7m5miX1vSBg?si=re70TIk3huKvbH9y" 
-                title="YouTube video player" 
-                frameBorder="0" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                referrerPolicy="strict-origin-when-cross-origin" 
-                allowFullScreen
-              ></iframe>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/0fhk4ffiugE?si=jxX2bIQjzaBATI8k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
           </div>
         </div>
